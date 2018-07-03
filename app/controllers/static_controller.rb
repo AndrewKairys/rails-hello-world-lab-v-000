5648440edd5cfe2f8f0eb527bbb0b74f2f947302
Andrew Kairys <andrewkairys@gmail.com>
2018-07-03 20:22:22 +0000

    automatically backed up by learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  def hello_world
+  def Hello_World
    render "hello_world"
  end
 end
